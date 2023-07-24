a4b3efe25cc292e78087df7a81f966b59a33decc
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-07-24 10:05:51 +0530

    First project done

---
+++ NikeFrontend/src/components/HeroSection.jsx
@@ -0,0 +1,43 @@
+
+
+const HeroSection = () => {
+
+    return <main className="hero container">
+    <div className="hero-content">
+
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <p>
+    YOUR FEET DESERVE THE BEST
+    YOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BEST
+    YOUR FEET DESERVE THE BEST
+    YOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BEST
+    YOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BEST
+
+    </p>
+
+    <div className="hero-btn">
+    <button>Shop Now</button>
+    <button className="secondary-btn"> Category</button>
+
+    </div>
+    
+    <div className="shopping">
+        <p>
+            Also available on
+        </p>
+        <div className="brand-icons">
+        <img src="/images/amazon.png" alt="amazon.logo" />
+        <img src="/images/flipkart.png" alt="flipkart.logo" />
+        </div>
+    </div>
+
+    </div>
+
+    <div className="hero-image">
+    <img src="/images/shoe_image.png" alt="shoe.logo" />
+    </div>
+
+    </main>
+};
+
+export default HeroSection;
